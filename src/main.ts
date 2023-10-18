c7e3c46e9653160f29c25fd4cdf12a6852cfeb21
denis rotaru <taypeh@gmail.com>
2023-10-18 23:24:38 +0300

    Moved from react to vanilla-ts

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { Game } from './models/game';
+import './style.css'
+
+window.addEventListener('load', () => {
+  //canvas setup
+  const canvas = document.getElementById('canvas1') as HTMLCanvasElement;
+  const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+  canvas.width = 1500;
+  canvas.height = 500;
+
+  const game = new Game(canvas.width, canvas.height);
+  let lastTime = 0;
+
+  //animation loop
+  function animate(timeStamp: number) {
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.update(deltaTime);
+    game.draw(ctx);
+    requestAnimationFrame(animate);
+  }
+
+  animate(0);
+})
